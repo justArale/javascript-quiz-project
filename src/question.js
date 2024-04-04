9a1a8ae2d5938a841cf4d38ce2252b75fd757314
justArale <just1arale@gmail.com>
2024-04-04 07:42:33 +0200

    done  day 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,20 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    let currentIndex = this.choices.length;
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [this.choices[currentIndex], this.choices[randomIndex]] = [
+        this.choices[randomIndex],
+        this.choices[currentIndex],
+      ];
+    }
+  }
 }
